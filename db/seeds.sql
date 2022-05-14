6e76e4be88625dfa10be91078916e901d448e35a
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-14 17:11:32 -0400

    mysql errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,10 +8,21 @@ VALUES
 ('Quality');
 
 INSERT INTO role (title, salary, department_id) 
-    -> VALUES
-    -> ('Accountant', 30000, 3),
-    -> ('Quality Tech', 15000, 2),
-    -> ('Project Manager', 60000, 5),
-    -> ('Sales Rep', 50000, 7),
-    -> ('Marketing Coordinator', 80000, 6),
-    -> ('Account Executive', 70000, 1);
+ VALUES
+ ('Accountant', 30000, 3),
+ ('Quality Tech', 15000, 2),
+ ('Project Manager', 60000, 5),
+ ('Sales Rep', 50000, 7),
+ ('Marketing Coordinator', 80000, 6),
+ ('Account Executive', 70000, 1);
+
+ INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ VALUES
+ ('Reagan', 'Foust', 1, 1),
+ ('Logan', 'Foust', 4, null),
+ ('Kooper', 'Foust', 2, null),
+ ('Destiny', 'Moore', 3, 3),
+ ('Tom', 'Simmons', 5, null),
+ ('Mike', 'Webster', 7, 1),
+ ('Jessica', 'Cooper', 6, 6),
+ ('Naomi', 'Smith', 8, null);

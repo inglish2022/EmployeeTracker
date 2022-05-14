697fea6b18c5e7301f03accd85b86bc89641cb73
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-13 21:49:30 -0400

    role

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,11 @@ CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role  (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  INDEX dep_ind (department_id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,12 @@ VALUES
 ('Legal'),
 ('Accounting'),
 ('Quality');
+
+INSERT INTO role (title, salary, department_id) 
+    -> VALUES
+    -> ('Accountant', 30000, 3),
+    -> ('Quality Tech', 15000, 2),
+    -> ('Project Manager', 60000, 5),
+    -> ('Sales Rep', 50000, 7),
+    -> ('Marketing Coordinator', 80000, 6),
+    -> ('Account Executive', 70000, 1);

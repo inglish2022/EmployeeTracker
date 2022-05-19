221acc856069f3db606888a71c7b2d21e5f70f1d
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-19 15:01:04 -0400

    fix errors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const connection = mysql.createConnection(
 
 );
 
- afterConnection = () =>  {
+afterConnection = () => {
     promptUser();
 };
 
@@ -41,7 +41,7 @@ const connection = mysql.createConnection(
 const promptUser = () => {
     inquirer.prompt([
         {
-            
+
             type: 'list',
             name: 'choices',
             message: 'What would you like to do?',
@@ -116,7 +116,9 @@ showDepartments = () => {
 //showing all roles
 showRoles = () => {
     console.log('All roles.../n');
-    const sql = `SELECT role.id, role.title, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id`;
+    const sql = `SELECT role.id, 
+                    role.title, 
+                    department.name AS department FROM role INNER JOIN department ON role.department_id = department.id`;
 
     connection.query(sql, (err, rows) => {
         if (err) throw err;
@@ -593,150 +595,3 @@ viewBudget = () => {
 
 promptUser()
 
-
-
-
-
-
-
-
-// db.query(`SELECT * FROM department`, (err, rows) => {
-//     console.log(rows);
-// });
-
-// GET a single candidate
-// db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
-
-// Delete a candidate
-// db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result);
-//   });
-
-// Create a candidate
-// const sql = `INSERT INTO department (id, name) 
-//               VALUES (?,?)`;
-// const params = [1, 'Sales'];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-// Get all departments
-// app.get('/api/department', (req, res) => {
-//     const sql = `SELECT * FROM department`;
-
-//     db.query(sql, (err, rows) => {
-//         if (err) {
-//             res.status(500).json({ error: err.message });
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: rows
-//         });
-//     });
-// });
-
-// // Get a single candidate
-// app.get('/api/department/:id', (req, res) => {
-//     const sql = `SELECT * FROM department WHERE id = ?`;
-//     const params = [req.params.id];
-
-//     db.query(sql, params, (err, row) => {
-//         if (err) {
-//             res.status(400).json({ error: err.message });
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: row
-//         });
-//     });
-// });
-
-// // Delete a candidate
-// app.delete('/api/department/:id', (req, res) => {
-//     const sql = `DELETE FROM department WHERE id = ?`;
-//     const params = [req.params.id];
-
-//     db.query(sql, params, (err, result) => {
-//         if (err) {
-//             res.statusMessage(400).json({ error: res.message });
-//         } else if (!result.affectedRows) {
-//             res.json({
-//                 message: 'Department not found'
-//             });
-//         } else {
-//             res.json({
-//                 message: 'deleted',
-//                 changes: result.affectedRows,
-//                 id: req.params.id
-//             });
-//         }
-//     });
-// });
-
-// // Create a candidate
-// app.post('/api/department', ({ body }, res) => {
-//     const errors = inputCheck(body, 'name');
-//     if (errors) {
-//         res.status(400).json({ error: errors });
-//         return;
-//     }
-// });
-
-// const sql = `INSERT INTO department (name)
-//   VALUES (?)`;
-// const params = [body.name];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         res.status(400).json({ error: err.message });
-//         return;
-//     }
-//     res.json({
-//         message: 'success',
-//         data: body
-//     });
-// });
-
-// Default response for any other request (Not Found)
-// app.use((req, res) => {
-//     res.status(404).end();
-// });
-
-
-
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
